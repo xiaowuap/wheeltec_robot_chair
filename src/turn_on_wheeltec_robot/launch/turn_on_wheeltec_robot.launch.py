f9d451ef3b359d188d9767a128d1c45a2a8b4525
Enze Wu <wez@wez.ink>
2025-08-14 21:55:06 +0800

    调整静态变换发布器参数和激光雷达配置，优化地图更新间隔及最小旅行距离

--- src/turn_on_wheeltec_robot/launch/turn_on_wheeltec_robot.launch.py
+++ src/turn_on_wheeltec_robot/launch/turn_on_wheeltec_robot.launch.py
@@ -58,7 +58,7 @@ def generate_launch_description():
             package='tf2_ros', 
             executable='static_transform_publisher', 
             name='base_to_gyro',
-            arguments=['0', '0', '0','0', '0','0','base_footprint','gyro_link'],
+            arguments=['27', '0', '0','0', '0','0','base_footprint','gyro_link'],
     )
     
     imu_filter_node =  launch_ros.actions.Node(
